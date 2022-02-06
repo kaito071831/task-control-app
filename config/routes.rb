b78ff73a912b65f1ded24599a4ee583d1c534038
kaito071831 <kaito071831@gmail.com>
2022-02-06 20:44:22 +0900

    ルーティング書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  get 'group/index'
-  get 'group/edit'
-  get 'group/new'
-  get 'index/edit'
-  get 'index/new'
-  get 'group/index'
-  get 'group/show'
-  get 'group/edit'
-  get 'group/update'
-  get 'group/create'
-  get 'group/new'
-  get 'group/destroy'
+
   root 'pages#index'
   get 'pages/show'
   devise_for :users
+
+  resources :boardgroup do
+    resources :board do
+      resources :task
+    end
+    
+    resources :team
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
