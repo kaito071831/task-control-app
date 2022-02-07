9e82beb8657f7ceeb432f6c8d0b591a7eeb37252
kaito071831 <kaito071831@gmail.com>
2022-02-07 14:58:31 +0900

    Create process new and create action

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -4,8 +4,31 @@ class BoardController < ApplicationController
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boardgroup_path(@board.boardgroup_id), notice: "Success"
+    else
+      flash.now[:alert] = "Failed"
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :boardgroup_id)
   end
 end
