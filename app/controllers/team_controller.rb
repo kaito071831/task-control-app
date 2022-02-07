0c3018358917960e74a9ce07223133a24382c852
kaito071831 <kaito071831@gmail.com>
2022-02-07 21:17:42 +0900

    Create show, new and create action

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,19 +1,28 @@
 class TeamController < ApplicationController
-  def index
+  def show
+    @teams = Team.where(boardgroup_id: params[:boardgroup_id])
   end
 
   def new
+    @team = Team.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to boardgroup_path(params[:boardgroup_id]), notice: "Success"
+    else
+      flash.now[:alert] = "Failed"
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:user_id, :boardgroup_id)
   end
 end
--- test/controllers/team_controller_test.rb
+++ test/controllers/team_controller_test.rb
@@ -1,18 +1,13 @@
 require "test_helper"
 
 class TeamControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get team_index_url
+  test "should get show" do
+    get team_show_url
     assert_response :success
   end
 
   test "should get new" do
     get team_new_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get team_edit_url
     assert_response :success
   end
 end
