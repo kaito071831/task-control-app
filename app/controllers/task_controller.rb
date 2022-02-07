9f8ef089c17283961e0074ab01bbd07ccdf2f50f
kaito071831 <kaito071831@gmail.com>
2022-02-07 17:50:08 +0900

    Create process new and create actions

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,13 +4,18 @@ class TaskController < ApplicationController
   end
 
   def new
-    @board = Board.find(params[:board_id])
     @task = Task.new
+    @statuses = Status.all
   end
 
   def create
-    Task.create(task_params)
-    redirect_to action: "index"
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to boardgroup_board_path(params[:boardgroup_id], @task.board_id), notice: "Success"
+    else
+      flash.now[:alert] = "Failed"
+      render :new
+    end
   end
 
   def edit
@@ -19,6 +24,6 @@ class TaskController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :detail, :dead_line, :status_id).merge(board_id: params[:board_id])
+    params.require(:task).permit(:title, :detail, :dead_line, :status_id, :board_id)
   end
 end
