b4ca06d9ccfd9aceb9f4949e15f403e13814944b
kaito071831 <kaito071831@gmail.com>
2022-04-02 07:19:12 +0900

    Made the host name an environment variable.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "https://task-control-app.herokuapp.com/", protocol: "https" }
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'], protocol: "https" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
